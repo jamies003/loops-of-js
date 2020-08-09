e93967bbd4c69f6b70525ec2add15016bafc59f3
Jamie Stewart <stwjme@gmail.com>
2020-08-08 23:31:07 -0400

    loops

---
+++ loops010.js
@@ -0,0 +1,15 @@
+let arr = [31, 27, 13, 7, 41, 25];
+let min = Infinity
+let secondMin = Infinity; 
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+        secondMin = min;
+        min = arr[i]; 
+    } else if (arr[i]< secondMin) {
+        secondMin = arr[i]; 
+    }
+}
+
+console.log('Smallest number: ' + min);
+console.log('Second smallest number: ' + secondMin);
